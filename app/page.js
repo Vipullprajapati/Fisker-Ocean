97b8250a4fc03c59c8c5e1263bd86d630d600119
Vipul Prajapati <vipulprajapati0070@gmail.com>
2024-12-23 14:41:19 +0000

    bun installation done

---
+++ app/page.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+export const metadata = {
+  title: "Fisker Ocean",
+  description: "Fisker Ocean",
+};
+
+function Page() {
+  return (
+    <html>
+      <head>
+      </head>
+      <body>
+        <h1>about</h1>
+      </body>
+    </html>
+  );
+}
+
+export default Page;
