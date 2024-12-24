61071002cd589e9a9fa1446db8b191f848a00754
Vipul Prajapati <vipulprajapati0070@gmail.com>
2024-12-24 14:25:49 +0000

    In pages import all Components

--- app/page.js
+++ app/page.js
@@ -1,21 +1,16 @@
 "use client";
-import Header from "@/header";
+import Header from "@/components/Header";
 import Preview from "@/components/Preview";
 import Configurator from "@/components/Configurator";
 
-
-
-
-
-
 function Home() {
   return (
-    <div onContextMenu={(e) => e.preventDefault()}>
+    <div onContextMenu={(e) => e.preventDefault()} className="min-h-screen">
       <Header />
       <Preview />
-      <Configurator />      
-
+      <Configurator />
     </div>
-  )
+  );
 }
+
 export default Home;
--- components/Header.jsx
+++ components/Header.jsx
@@ -1,15 +1,23 @@
-import { RiGlobeLine } from "react-icons/ri";
+import Image from "next/image";
+import { RiGlobalLine } from "react-icons/ri";
 
 function Header() {
   return (
-    <header className=" bg-stone-200 text-stone-800 shadow-md">
-      <section>
-        <p>All Electric &amp; Fisker</p>
+    <header>
+      <section className="mb-2 bg-gray-200 py-1 text-center text-sm font-medium">
+        <p>All electric &#x2022; Dynamic &#x2022; Driver Range</p>
       </section>
-      <section className="mx-auto flex max-w-7xl justify-between ">
-        <img src="public/fisker.svg" alt="fisker" />
-        <div>
-          <RiGlobeLine className="text-stone-800" />
+      <section className="mx-auto flex max-w-7xl justify-between">
+        <Image
+          src="fisker.svg"
+          alt="Fisker Logo"
+          height={100}
+          width={100}
+          className="w-12 cursor-pointer duration-100 hover:scale-110 sm:w-20"
+        />
+        <div className="flex items-center gap-1 text-stone-700">
+          <RiGlobalLine className="text-xl" />
+          <p>US</p>
         </div>
       </section>
     </header>
