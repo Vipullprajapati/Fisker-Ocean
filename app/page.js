2ac7f5d47f534cf0f0650ba63a607dfeb9431c87
Vipul Prajapati <vipulprajapati0070@gmail.com>
2024-12-26 14:44:35 +0000

    Add interiorMap $ Show Interior Info

--- app/page.js
+++ app/page.js
@@ -1,21 +1,35 @@
 "use client";
+import { useState } from "react";
 import Header from "@/components/Header";
 import Preview from "@/components/Preview";
 import Configurator from "@/components/Configurator";
 
 function Home() {
+  const [state, setState] = useState({
+    exterior: "Black Pearl",
+    wheel: "Aerostealth",
+    interior: "Black",
+  });
+
+  const handleStateChange = (option, value) => {
+    setState((prevState) => ({
+      ...prevState,
+      [option]: value,
+    }));
+  };
+
   return (
     <div
       onContextMenu={(e) => e.preventDefault()}
       className="mb-5 min-h-screen"
     >
       <Header />
-      <main className="mx-auto flex w-full max-w-7xl flex-col gap-2 px-2 sm:flex-row sm:gap-4">
-        <section className="w-fullsm:w-3/4">
-          <Preview />
+      <main className="mx-auto flex w-full max-w-7xl flex-col gap-2 px-2 md:flex-row md:gap-4">
+        <section className="w-full md:w-3/4">
+          <Preview {...state} />
         </section>
-        <aside className="w-full bg-green-300 sm:w-1/4">
-          <Configurator />
+        <aside className="w-full md:w-1/4">
+          <Configurator onChange={handleStateChange} {...state} />
         </aside>
       </main>
     </div>
--- components/Preview.jsx
+++ components/Preview.jsx
@@ -1,12 +1,14 @@
 import Image from "next/image";
 
-function Preview() {
+import { imageMap, interiorMap } from "@/lib/imageMap";
+
+function Preview({ exterior, wheel, interior }) {
   return (
     <>
       {/* Exterior Image */}
 
       <Image
-        src="/images/black_pearl/aerostealth.webp"
+        src={imageMap[exterior].wheels[wheel]}
         alt=""
         width={0}
         height={0}
@@ -16,18 +18,25 @@ function Preview() {
 
       {/* Exterior Description */}
       <section className="my-4 pl-2">
-        <h3 className="text-2xl font-bold">Metallic Gloss</h3>
-        <p>A rich and luxurious interior with a modern and sleek design.</p>
+        <h3 className="text-2xl font-bold">
+          {exterior}: {imageMap[exterior].finish} w/ {wheel}
+        </h3>
+        <p>{imageMap[exterior].description}</p>
       </section>
       {/* Interior Image */}
+
       <Image
-        src="/images/black_pearl/slipstream_black.webp"
+        src={interiorMap[interior]}
         alt=""
         width={0}
         height={0}
         sizes="100%"
         className="h-96 w-full rounded-lg object-cover sm:h-[500px]"
       />
+
+      <section className="mt-4 pl-2">
+        <h3 className="text-xl font-bold">Interior {interior}</h3>
+      </section>
     </>
   );
 }
